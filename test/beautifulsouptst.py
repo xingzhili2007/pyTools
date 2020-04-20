226fcf5f3e437323e5267011602c47c4f27cbe23
李行之 <xingzhili2007@163.com>
2020-04-20 11:35:08 +0800

    Create beautifulsouptst.py

---
+++ test/beautifulsouptst.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+import requests
+from bs4 import BeautifulSoup  # 从bs4这个库中导入BeautifulSoup
+
+link = "https://moive.qupeiyin.com/home/show/share/sharefrom/oneself/id/MDAwMDAwMDAwMLF3qqyAsbrbr7eArw"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+r = requests.get(link, headers=headers)
+
+soup = BeautifulSoup(r.text, "html.parser")  # 使用BeautifulSoup解析
+
+# 找到第一篇文章标题，定位到class是"post-title"的h1元素，提取a，提取a里面的字符串，strip()去除左右空格
+result = soup.find('video')
+print(result.attrs['src'])
