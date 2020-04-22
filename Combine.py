842ac6ca0a486e4b33fd7658edb4c11f5d48f0fe
李行之 <xingzhili2007@163.com>
2020-04-22 16:19:26 +0800

    Update Combine.py

--- Combine.py
+++ Combine.py
@@ -11,8 +11,11 @@ gets = Get_URLinfo()
 def Combine_English_qupeiyin(URL, store_path):
     Video_URL = gets.English_qupeiyin(URL)
     if store_path == '':
-        default = os.getcwd()+'/Cache'
-        if not os.path.exists(default):
-            os.makedirs(default)
-        store_path = default
+        store_path = os.getcwd()+'/Cache'
+        if not os.path.exists(store_path):
+            os.makedirs(store_path)
+    nowtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    nowtime = str(nowtime)
+    store_path = store_path + '/'+nowtime
+    os.makedirs(store_path)
     gets.download(store_path, Video_URL)
