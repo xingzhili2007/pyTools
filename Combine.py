3b6e1ccd4df86fae4f511dac8ff3e89baaa27a91
xingzhili2007 <xingzhili2007@163.com>
2020-07-02 22:21:42 +0800

    new

--- Combine.py
+++ Combine.py
@@ -1,6 +1,6 @@
 from MyTools import *
-from myMemberList import *  # Webhook地址文件
-from myMemberSec import *  # 密钥文件
+from MemberList import *  # Webhook地址文件
+from MemberSec import *  # 密钥文件
 
 # 定义类()
 robot = Dingtalk()
@@ -11,12 +11,12 @@ gets = Get_URLinfo()
 def Combine_English_qupeiyin(URL, store_path):
     Video_URL = gets.English_qupeiyin(URL)
     if store_path == '':
-        store_path = os.getcwd()+'/Cache'
+        store_path = os.getcwd() + '/Cache'
         if not os.path.exists(store_path):
             os.makedirs(store_path)
     nowtime = time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime(time.time()))
     nowtime = str(nowtime)
-    store_path = store_path + '/'+nowtime
+    store_path = store_path + '/' + nowtime
     os.makedirs(store_path)
     gets.download(store_path, Video_URL, '')
     print('Path:', store_path)
@@ -32,7 +32,7 @@ def Combine_English_qupeiyin(URL, store_path):
 def Combine_baiduttsURL(text, lan, spd, toplay, store_path, filename):
     URL = tool.baiduttsURL(text, lan, spd, toplay)
     if store_path == '':
-        store_path = os.getcwd()+'/Cache'
+        store_path = os.getcwd() + '/Cache'
         if not os.path.exists(store_path):
             os.makedirs(store_path)
     nowtime = time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime(time.time()))
@@ -42,7 +42,7 @@ def Combine_baiduttsURL(text, lan, spd, toplay, store_path, filename):
         os.makedirs(store_path)
     # download
     if filename == '':
-        filename = text+'&speed='+str(spd)+'.mp3'
+        filename = text + '&speed=' + str(spd) + '.mp3'
     filepath = os.path.join(store_path, filename)
     file_data = requests.get(URL, allow_redirects=True).content
     with open(filepath, 'wb') as handler:
@@ -54,7 +54,7 @@ def Combine_baiduttsURL(text, lan, spd, toplay, store_path, filename):
 def Combine_2wm(text, store_path, filename):
     URL = tool.txt2wm(text)
     if store_path == '':
-        store_path = os.getcwd()+'/Cache'
+        store_path = os.getcwd() + '/Cache'
         if not os.path.exists(store_path):
             os.makedirs(store_path)
     nowtime = time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime(time.time()))
@@ -64,7 +64,7 @@ def Combine_2wm(text, store_path, filename):
         os.makedirs(store_path)
     # download
     if filename == '':
-        filename = text+'.png'
+        filename = text + '.png'
     filepath = os.path.join(store_path, filename)
     file_data = requests.get(URL, allow_redirects=True).content
     with open(filepath, 'wb') as handler:
---
+++ envValue/Dingtalk/Member.py
@@ -0,0 +1,17 @@
+Library = {
+    '一年级12班(130班)': {
+        "Webhook":
+        "https://oapi.dingtalk.com/robot/send?access_token=4831e8e3bdfdec5129e6d642b10838e58abec5e480500a872a3d88a97d973362",
+        "Sec": [False],
+        "KeyWord": [4, '信息', '提示', '警告', '注意']
+    },
+    '一年级12班(130班)学生': {
+        "Webhook":
+        "https://oapi.dingtalk.com/robot/send?access_token=bfde62537453645a25574567f1c51a8b6db520c146ac66327100c7656771e94e",
+        "Sec": [
+            True,
+            'SEC836ce2bd8032e17d918cfb54a57345dc68ddc741be88e5d370f1e6568b1f9086'
+        ],
+        "KeyWord": [False]
+    }
+}
