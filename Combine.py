ca86b98f6833c0715630c509f226c8fc729b215d
xingzhili2007 <xingzhili2007@163.com>
2020-07-12 14:22:18 +0800

    0

--- Combine.py
+++ Combine.py
@@ -1,6 +1,7 @@
 from MyTools import *
 from MemberList import *  # Webhook地址文件
 from MemberSec import *  # 密钥文件
+from envValue.Dingtalk.Member import *
 
 # 定义类()
 robot = Dingtalk()
@@ -71,3 +72,23 @@ def Combine_2wm(text, store_path, filename):
         handler.write(file_data)
     print('Path:', store_path)
     return store_path
+
+
+def txtwebhookAuto(text, GroupName):
+    #检测目录中是否存在该机器人(群聊)
+    try:
+        webhook = Library[GroupName]['Webhook']  #如果有该机器人信息就赋值
+    except:
+        print('群名不存在')  #如果没有则报错
+        return False
+    #检测该机器人是否应用了加签验证
+    if Library[GroupName]['Sec'][0] == True:
+        webhook = robot.addticket(Library[GroupName]['Webhook'],
+                                  Library['机器人测试']['Sec'][1])  #如果应用则加签
+    #检测该机器人是否应用了关键字验证
+    if Library[GroupName]['KeyWord'][0] != 0:
+        KeyWord = Library[GroupName]['KeyWord'][1]  #如果应用则选择第一个关键字
+    else:
+        KeyWord = ''  #如果未应用则为空
+    text = str(text)  #格式化要发送的文字
+    robot.txtwebhook(text, webhook, '', '', '', '', KeyWord)  #发送
--- envValue/Dingtalk/Member.py
+++ envValue/Dingtalk/Member.py
@@ -1,17 +1,52 @@
+'''
+存储模版:
+Library:
+    #群聊标识符
+    'GroupName': {
+        #Webhook地址:
+        "Webhook":#类别名称
+        "https://oapi.dingtalk.com/robot/send?access_token=XXXXXXXXX"#类别内容
+        ,#分隔符
+        #机器人加签密钥:
+        "Sec": [],#类别名称:Sec-类别内容_格式:列表[bool(是否需要加签验证),str(密钥)]
+        "KeyWord": []#类别名称:KeyWord-类别内容_格式:列表[int(关键词的个数),str(关键词1),str(关键词2)......]
+    }
+'''
 Library = {
+    #群聊标识符
     '一年级12班(130班)': {
-        "Webhook":
-        "https://oapi.dingtalk.com/robot/send?access_token=4831e8e3bdfdec5129e6d642b10838e58abec5e480500a872a3d88a97d973362",
-        "Sec": [False],
-        "KeyWord": [4, '信息', '提示', '警告', '注意']
-    },
+        #Webhook地址:
+        "Webhook":#类别名称
+        "https://oapi.dingtalk.com/robot/send?access_token=4831e8e3bdfdec5129e6d642b10838e58abec5e480500a872a3d88a97d973362"#类别内容
+        ,#分隔符
+        #机器人加签密钥:
+        "Sec": [False],#类别名称:Sec-类别内容_格式:列表[bool(是否需要加签验证),str(密钥)]
+        "KeyWord": [4, '信息', '提示', '警告', '注意']#类别名称:KeyWord-类别内容_格式:列表[int(关键词的个数),str(关键词1),str(关键词2)......]
+    }
+
+    ,#分隔符
+
+    #群聊标识符
     '一年级12班(130班)学生': {
-        "Webhook":
-        "https://oapi.dingtalk.com/robot/send?access_token=bfde62537453645a25574567f1c51a8b6db520c146ac66327100c7656771e94e",
+        #Webhook地址:
+        "Webhook":#类别名称
+        "https://oapi.dingtalk.com/robot/send?access_token=bfde62537453645a25574567f1c51a8b6db520c146ac66327100c7656771e94e"  #类别内容
+        ,#分隔符
         "Sec": [
-            True,
-            'SEC836ce2bd8032e17d918cfb54a57345dc68ddc741be88e5d370f1e6568b1f9086'
+            True,#类别名称:Sec
+            'SEC836ce2bd8032e17d918cfb54a57345dc68ddc741be88e5d370f1e6568b1f9086'#类别内容_格式:列表[bool(是否需要加签验证),str(密钥)]
         ],
-        "KeyWord": [False]
+        "KeyWord": [False]#类别名称:KeyWord-类别内容_格式:列表[int(关键词的个数),str(关键词1),str(关键词2)......]
+    },
+
+    #群聊标识符
+    '机器人测试': {
+        #Webhook地址:
+        "Webhook":#类别名称
+        "https://oapi.dingtalk.com/robot/send?access_token=5c185ea0ae0e93ebd60b05d4704bbaff3dc120628cac9f0e1c6d3d648b0059f0"#类别内容
+        ,#分隔符
+        #机器人加签密钥:
+        "Sec": [True,'SEC18480f4e0c7b5c99db6eee1b3b364569f596ce9393110d454f18fd2bac58db92'],#类别名称:Sec-类别内容_格式:列表[bool(是否需要加签验证),str(密钥)]
+        "KeyWord": [False]#类别名称:KeyWord-类别内容_格式:列表[int(关键词的个数),str(关键词1),str(关键词2)......]
     }
 }
